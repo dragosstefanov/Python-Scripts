54d9fe4899485a88106858ddc2cb7deb21512319
dragosstefanov <stefanov.drag@gmail.com>
2018-12-29 12:53:15 -0500

    Update Phone_number_regex.py

--- Phone_number_regex.py
+++ Phone_number_regex.py
@@ -1,6 +1,6 @@
 import re
 
 phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-mo = phoneNumRegex.search('My number is 509-555-9911.')
+num = phoneNumRegex.search('My number is 509-555-9911.')
 
-print('Phone number: ' + mo.group())
+print('Phone number: ' + num.group())
